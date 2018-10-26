7b4ee8aca2178fa89720b3e9f6fd177299d2bff6
Jakub Starzyński <jakub.starzynski@norbsoft.com>
2018-10-26 16:16:27 +0200

    Adding Search Box

--- app/src/main/java/com/android/challenge/movies/ui/MainActivity.kt
+++ app/src/main/java/com/android/challenge/movies/ui/MainActivity.kt
@@ -4,10 +4,16 @@ import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.content.Intent
 import android.content.res.Resources
+import android.database.MatrixCursor
 import android.graphics.Point
 import android.os.Bundle
+import android.provider.BaseColumns
+import android.support.v4.widget.CursorAdapter
+import android.support.v4.widget.SimpleCursorAdapter
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.SearchView
+import android.view.Menu
 import android.view.View
 import com.android.challenge.movies.R
 import com.android.challenge.movies.repository.MoviesGridAdapter
@@ -20,11 +26,16 @@ class MainActivity : AppCompatActivity() {
     private val imageOptimalWidthDp = 100
     private val imageOptimalRatio = 16f/9f
 
+    private lateinit var searchViewAdapter: SimpleCursorAdapter
+
+
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initSearchAdapter()
 
         val columnSpan = calculateColumnSpan()
         val viewAdapter = MoviesGridAdapter(this, calculatePreferredHeight(columnSpan), 10) {
@@ -55,6 +66,32 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null)
             viewModel.startNowPlayingMoviesSearch()
+    }
+
+    private fun initSearchAdapter() {
+        searchViewAdapter = SimpleCursorAdapter(this,
+                android.R.layout.simple_list_item_1,
+                null,
+                arrayOf("title"),
+                intArrayOf(android.R.id.text1),
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_main_menu, menu)
+
+        val searchMenuItem = menu.findItem(R.id.search)
+
+        val actionView = (searchMenuItem.actionView as SearchView)
+            actionView.queryHint = getString(R.string.hint_query)
+            actionView.suggestionsAdapter = searchViewAdapter
+
+        val matrixCursor = MatrixCursor(arrayOf(BaseColumns._ID, "title"))
+        for (i in 0..10)
+            matrixCursor.addRow(arrayOf(i, "title$i"))
+        searchViewAdapter.changeCursor(matrixCursor)
+
+        return true
     }
 
     private fun calculateColumnSpan() = Point().run {
